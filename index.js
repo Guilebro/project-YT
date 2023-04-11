dec74265e095fef426479f171c921712d23a7ee4
Maxime Dupin <maximedupin@live.fr>
2023-04-12 00:02:55 +0200

    navigation style completed

--- index.js
+++ index.js
@@ -11,232 +11,260 @@ const videos = [
     id: 2,
     title: "Learn React in 30 minutes",
     picture: "https://i.ytimg.com/vi/Ke90Tje7VS0/maxresdefault.jpg",
-    view: 500000,
+    view: "1M de vues",
     editor: "Jane Smith",
-    time: "30:00",
+    time: "il y a 2 semaines",
   },
   {
     id: 3,
     title: "CSS Grid Layout Tutorial",
     picture: "https://i.ytimg.com/vi/7kVeCqQCxlk/maxresdefault.jpg",
-    view: 200000,
+    view: "1M de vues",
     editor: "Bob Johnson",
-    time: "22:45",
+    time: "il y a 2 semaines",
   },
   {
     id: 4,
     title: "How to Code a Basic HTML Website",
     picture: "https://i.ytimg.com/vi/QA0XpGhiz5w/maxresdefault.jpg",
-    view: 125874,
+    view: "1M de vues",
     editor: "John Doe",
-    time: "20:10",
+    time: "il y a 2 semaines",
   },
   {
     id: 5,
     title: "JavaScript Crash Course for Beginners",
     picture: "https://i.ytimg.com/vi/4deVCNJq3qc/maxresdefault.jpg",
-    view: 568974,
+    view: "1M de vues",
     editor: "Jane Smith",
-    time: "30:25",
+    time: "il y a 2 semaines",
   },
   {
     id: 6,
     title: "A-ha - Take On Me (Official 4K Music Video)",
     picture: "https://i.ytimg.com/vi/tCnBrrnOefs/hqdefault.jpg",
-    view: "1.9B",
+    view: "1M de vues",
     editor: "RHINO",
-    time: "4:04",
+    time: "il y a 2 semaines",
   },
   {
     id: 7,
     title: "React Native Tutorial - Building a Mobile App",
     picture: "https://i.ytimg.com/vi/MJzmZ9qmdaE/maxresdefault.jpg",
-    view: 135468,
+    view: "1M de vues",
     editor: "Sarah Lee",
-    time: "40:30",
+    time: "il y a 2 semaines",
   },
   {
     id: 8,
     title: "How to Build a RESTful API with Node.js",
     picture: "./assets/video.jpg",
-    view: 458796,
+    view: "1M de vues",
     editor: "Jack Wilson",
-    time: "28:50",
+    time: "il y a 2 semaines",
   },
   {
     id: 9,
     title: "Introduction to Python Programming",
     picture: "./assets/video.jpg",
-    view: 987654,
+    view: "1M de vues",
     editor: "Emily Park",
-    time: "45:15",
+    time: "il y a 2 semaines",
   },
   {
     id: 10,
     title: "Vue.js Tutorial - Build a Web App from Scratch",
     picture: "https://i.ytimg.com/vi/hdI2bqOjy3c/maxresdefault.jpg",
-    view: 741258,
+    view: "1M de vues",
     editor: "David Kim",
-    time: "50:05",
+    time: "il y a 2 semaines",
   },
   {
     id: 11,
     title: "Node.js Authentication Tutorial",
     picture: "https://i.ytimg.com/vi/2jqok-WgelI/maxresdefault.jpg",
-    view: 369852,
+    view: "1M de vues",
     editor: "Linda Chen",
-    time: "22:40",
+    time: "il y a 2 semaines",
   },
   {
     id: 12,
     title: "Docker Tutorial for Beginners",
     picture: "https://i.ytimg.com/vi/fqMOX6JJhGo/maxresdefault.jpg",
-    view: 963258,
+    view: "1M de vues",
     editor: "Peter Lee",
-    time: "35:55",
+    time: "il y a 2 semaines",
   },
   {
     id: 13,
     title: "Angular Tutorial - Build a Web App from Scratch",
     picture: "./assets/video.jpg",
-    view: 159753,
+    view: "1M de vues",
     editor: "Mary Brown",
-    time: "55:20",
+    time: "il y a 2 semaines",
   },
   {
     id: 14,
     picture: "./assets/video.jpg",
     title: "How to beat Elden Ring with one finger!!!",
     editor: "Test",
-    views: "10K",
-    time: "test",
+    view: "1M de vues",
+    time: "il y a 2 semaines",
   },
   {
     id: 15,
     picture: "./assets/video.jpg",
     title: "Hello World Sucks",
     editor: "Guilebro Code",
-    views: "10K",
-    time: "test",
+    view: "1M de vues",
+    time: "il y a 2 semaines",
   },
 ];
 
 const mainSection = [
-    {
-        id: 1,
-        title: "Accueil",
-    },
-    {
-        id: 2,
-        title: "Shorts",
-    },
-    {
-        id: 3,
-        title: "Abonnements",
-    },
+  {
+    id: 1,
+    title: "Accueil",
+    icon: "./assets/nav-icon.png",
+  },
+  {
+    id: 2,
+    title: "Shorts",
+    icon: "./assets/nav-icon.png",
+  },
+  {
+    id: 3,
+    title: "Abonnements",
+    icon: "./assets/nav-icon.png",
+  },
 ];
 
-    const privateSection = [
+const privateSection = [
   {
     id: 1,
     title: "Bibliothèque",
+    icon: "./assets/nav-icon.png",
   },
   {
     id: 2,
     title: "Historique",
+    icon: "./assets/nav-icon.png",
   },
   {
     id: 3,
     title: "Vos vidéos",
+    icon: "./assets/nav-icon.png",
   },
   {
     id: 4,
     title: "À regarder plus tard",
+    icon: "./assets/nav-icon.png",
   },
   {
     id: 5,
     title: "Vidéos J'aime",
+    icon: "./assets/nav-icon.png",
   },
-    ];
+];
 
-     const followSection = [
-       {
-         id: 1,
-         title: "Editeur1",
-       },
-       {
-         id: 2,
-         title: "Editeur2",
-       },
-       {
-         id: 3,
-         title: "Editeur3",
-       },
-       {
-         id: 4,
-         title: "Editeur4",
-       },
-       {
-         id: 5,
-         title: "Editeur5",
-       },
-       {
-         id: 6,
-         title: "Editeur6",
-       },
-       {
-         id: 7,
-         title: "Editeur7",
-       },
-       {
-         id: 8,
-         title: "Editeur8",
-       },
-       {
-         id: 9,
-         title: "Editeur9",
-       },
-       {
-         id: 10,
-         title: "Editeur10",
-       },
-     ];
+const followSection = [];
+for (let i = 1; i < 11; i++) {
+  followSection.push({
+    id: i,
+    title: "Editeur" + i,
+  });
+};
+
+const explorerSection = [
+  {
+    id: 1,
+    title: "Tendances",
+    icon: "./assets/nav-icon.png",
+  },
+  {
+    id: 2,
+    title: "Musique",
+    icon: "./assets/nav-icon.png",
+  },
+  {
+    id: 3,
+    title: "Films et TV",
+    icon: "./assets/nav-icon.png",
+  },
+  {
+    id: 4,
+    title: "Direct",
+    icon: "./assets/nav-icon.png",
+  },
+  {
+    id: 5,
+    title: "Jeux vidéos",
+    icon: "./assets/nav-icon.png",
+  },
+  {
+    id: 6,
+    title: "Actualités",
+    icon: "./assets/nav-icon.png",
+  },
+  {
+    id: 7,
+    title: "Sport",
+    icon: "./assets/nav-icon.png",
+  },
+  {
+    id: 8,
+    title: "Savoirs & Cultures",
+    icon: "./assets/nav-icon.png",
+  },
+  {
+    id: 9,
+    title: "Mode et beauté",
+    icon: "./assets/nav-icon.png",
+  },
+];
 
 const myDiv = document.querySelector(".videos-container");
 const mainSectionUl = document.querySelector(".main-section-ul");
 const privateSectionUl = document.querySelector(".private-section-ul");
 const followSectionUl = document.querySelector(".follow-section-ul");
+const explorerSectionUl = document.querySelector(".explorer-section-ul");
 const myInput = document.querySelector(".input-search");
 const divResults = document.querySelector(".results");
 
-
- videos.map((element) => {
-    myDiv.innerHTML += `<div class="each-video-card">
+videos.map((element) => {
+  myDiv.innerHTML += `<div class="each-video-card">
                         <img class="picture-video-card" src="${element.picture}" alt="${element.id}"/>
-                        <span>${element.title}</span>
-                        <span>${element.editor}</span>
+                        <span class="video-title">${element.title}</span>
+                        <span class="editor-view-time-style">${element.editor}</span>
                         <div class="view-time-card">
-                        <span>${element.view}</span>
-                        <span>${element.time}</span>
+                        <span class="editor-view-time-style">${element.view} • ${element.time}</span>
+                        <span class="editor-view-time-style"></span>
                         </div>
                         </div>`;
-  });
+});
 
 mainSection.map((element) => {
-    mainSectionUl.innerHTML += `<li>${element.title}</li>`;
+  mainSectionUl.innerHTML += `<li class="space-lines">${element.title}</li>`
 });
 
 privateSection.map((element) => {
-    privateSectionUl.innerHTML += `<li>${element.title}</li>`;
+  privateSectionUl.innerHTML += `<li class="space-lines">${element.title}</li>`;
 });
 
+followSection.map((element) => {
+  followSectionUl.innerHTML += `<li class="space-lines">${element.title}</li>`;
+});
+
+explorerSection.map((element) => {
+  explorerSectionUl.innerHTML += `<li class="space-lines">${element.title}</li>`
+});
 
 function allVideos(videos) {
   divResults.innerHTML = "";
   videos.map((element) => {
-    divResults.innerHTML += `<ul><li>${element.title}</li></ul>`;});
-}; 
-
+    divResults.innerHTML += `<div class="div-display"><span class="span-display">🔍\xa0\xa0\xa0${element.title}</span></div>`;
+  });
+}
 
 myInput.addEventListener("input", (e) => {
   let searchTerm = e.target.value;
@@ -247,12 +275,10 @@ myInput.addEventListener("input", (e) => {
       element.title.toLowerCase().includes(searchTerm.toLowerCase())
     );
     allVideos(filteredSearch);
-     if (filteredSearch.length === 0) {
-       divResults.innerHTML += `<p>Aucun résultats<p>`;
-     }
+    if (filteredSearch.length === 0) {
+      divResults.innerHTML += `<p class="span-display">Aucun résultat<p>`;
+    }
   } else {
     divResults.style.display = "none";
-  };
+  }
 });
-
-
